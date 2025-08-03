1f4d7c5479202041edc7c017bac8de7524adcb24
Abhay Mehta <114793909+Abhaymehta1424@users.noreply.github.com>
2025-08-03 23:12:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxSum(int arr[]) {
+        // code here
+        int n = arr.length;
+        int prev_prev = 0, prev = 0;
+        for(int i=0; i<n; i++){
+            int curr = Math.max(prev, prev_prev+arr[i]);
+            prev_prev = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
